0a0f37418c9013c292fa14693be45eef55592ff8
Ahmet Aşık <ahmet9417@gmail.com>
2020-09-28 17:24:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ eval $(minikube docker-env)
 docker build -t gocache-client .
 kubectl delete deployment gocache-client
 kubectl delete service gocache-client
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
+kubectl create -f deployment.yml
+kubectl create -f service.yml
 minikube service gocache-client
